ad11d1a7e4f1cdd7490651761a5b8edc91e4ca68
marb61a <marb61a@gmail.com>
2018-07-05 16:57:59 +0100

    Added Qrcode views, model

---
+++ app/Models/Qrcode.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @SWG\Definition(
+ *      definition="Qrcode",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="user_id",
+ *          description="user_id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="website",
+ *          description="website",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="company_name",
+ *          description="company_name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="product_name",
+ *          description="product_name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="product_url",
+ *          description="product_url",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="callback_url",
+ *          description="callback_url",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="qrcode_path",
+ *          description="qrcode_path",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="amount",
+ *          description="amount",
+ *          type="number",
+ *          format="float"
+ *      ),
+ *      @SWG\Property(
+ *          property="status",
+ *          description="status",
+ *          type="boolean"
+ *      )
+ * )
+ */
+class Qrcode extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'qrcodes';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'user_id',
+        'website',
+        'company_name',
+        'product_name',
+        'product_url',
+        'callback_url',
+        'qrcode_path',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'website' => 'string',
+        'company_name' => 'string',
+        'product_name' => 'string',
+        'product_url' => 'string',
+        'callback_url' => 'string',
+        'qrcode_path' => 'string',
+        'amount' => 'float',
+        'status' => 'boolean'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
