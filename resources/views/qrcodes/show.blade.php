8a771d1a218cedc6cf4765ea9f5ef299144d6b2c
marb61a <marb61a@gmail.com>
2018-07-30 16:51:36 +0100

    updated show.blade view

--- resources/views/qrcodes/show.blade.php
+++ resources/views/qrcodes/show.blade.php
@@ -2,16 +2,25 @@
 
 @section('content')
     <section class="content-header">
-        <h1>
-            Qrcode
+        <h1 class="pull-left"></h1>
+        <h1 class="pull-right">
+            @if(!Auth::guest() && ($qrcode->user_id == Auth::user()->id || Auth::user()->role_id < 3))
+               <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('qrcodes.edit', [$qrcode->id]) !!}">
+                Edit
+               </a>               
+            @endif
         </h1>
     </section>
-    <div class="content">
+    <div class="content" >
+        <div class="clearfix"></div>
+
+            @include('flash::message')
+
+        <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                <div class="row" style="padding-left: 20px">
+                <div class="row" style="padding-left: 20px" >
                     @include('qrcodes.show_fields')
-                    <a href="{!! route('qrcodes.index') !!}" class="btn btn-default">Back</a>
                 </div>
             </div>
         </div>
