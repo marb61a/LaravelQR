757211eeab451aa2ef673c59aa834d77382e8a7c
marb61a <marb61a@gmail.com>
2018-07-30 17:47:23 +0100

    added admin and moderator roles to menu.blade

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -23,3 +23,32 @@
 		</a>
 	</li>
 @endif
+
+
+{{-- Moderators --}}
+@if(Auth::user()->role_id < 3)
+<li class="{{ Request::is('users*') ? 'active' : '' }}">
+    <a href="{!! route('users.index') !!}">
+    	<i class="fa fa-edit"></i><span>Users</span>
+    </a>
+</li>
+<li class="{{ Request::is('accounts*') ? 'active' : '' }}">
+    <a href="{!! route('accounts.index') !!}">
+    	<i class="fa fa-edit"></i><span>Accounts</span>
+    </a>
+</li>  
+<li class="{{ Request::is('accountHistories*') ? 'active' : '' }}">
+    <a href="{!! route('accountHistories.index') !!}">
+    	<i class="fa fa-edit"></i><span>Account Histories</span>
+    </a>
+</li>
+@endif
+
+{{-- Admin --}}
+@if(Auth::user()->role_id == 1)
+<li class="{{ Request::is('roles*') ? 'active' : '' }}">
+    <a href="{!! route('roles.index') !!}">
+    	<i class="fa fa-edit"></i><span>Roles</span>
+    </a>
+</li>
+@endif
